70b338b2477a589680c241521b3c789f751ae829
KingRayhan <rayhan095@gmail.com>
2021-07-20 06:31:26 +0600

    ep12

---
+++ reddit-api/database/migrations/2021_07_20_001358_create_threads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('url')->nullable();
+            $table->string('text')->nullable();
+            $table->string('attachment')->nullable();
+            $table->enum('attachment_type', ['IMAGE', 'VIDEO']);
+            $table->enum('thread_type', ['TEXT', "LINK"]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
