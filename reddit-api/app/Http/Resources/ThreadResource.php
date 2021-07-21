9dea86d8177c9bd50d47c97baec6dc69978031ef
KingRayhan <rayhan095@gmail.com>
2021-07-22 04:03:29 +0600

    Simplify Thread Resource with array_merge()

--- reddit-api/app/Http/Resources/ThreadResource.php
+++ reddit-api/app/Http/Resources/ThreadResource.php
@@ -7,22 +7,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 class ThreadResource extends JsonResource
 {
 
-    /*
-     *
-     * "id": 101,
-            "title": "Voluptas hic et labore et.",
-            "slug": "voluptas-hic-et-labore-et--1626899464",
-            "url": "https://keebler.com/sapiente-nostrum-architecto-perferendis-deserunt-tempora.html",
-            "text": null,
-            "attachment": null,
-            "user_id": 6,
-            "attachment_type": null,
-            "thread_type": "LINK",
-            "created_at": "2021-07-21T20:31:04.000000Z",
-            "updated_at": "2021-07-21T20:31:04.000000Z"
-     */
-
-
     /**
      * Transform the resource into an array.
      *
@@ -31,12 +15,16 @@ class ThreadResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-          'title' => $this->title,
-            'slug' => $this->slug,
-            'url' => $this->url,
-            'text' => $this->text,
-            "user" => new UserResource($this->user)
-        ];
+        $default =  parent::toArray($request);
+        unset($default['user_id']);
+        unset($default['created_at']);
+        unset($default['updated_at']);
+
+
+        return array_merge($default , [
+            'submittedAt' => $this->created_at->diffForHumans(),
+            'user' => new UserResource($this->user)
+        ]);
+
     }
 }
